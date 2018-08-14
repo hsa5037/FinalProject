c66f90780f5dee9a4c5472c16caf9926c2ee014d
Humza Ahmed <ahmedh@eaa-humza-ahmed-3.local>
2018-08-14 09:18:39 -0400

    Initial Commit

---
+++ FinalProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProject
+//
+//  Created by Humza Ahmed on 8/14/18.
+//  Copyright © 2018 Humza Ahmed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
